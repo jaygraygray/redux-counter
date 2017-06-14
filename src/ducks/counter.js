64b5ff6caa4ea4ff8ee3de5577d2bc33d800ac46
Jeremy Gray <jeremygray001@gmail.com>
2017-06-14 10:59:49 -0600

    Set up store and reducer

--- src/ducks/counter.js
+++ src/ducks/counter.js
@@ -1,2 +1,12 @@
 // change this to true to see test results on the black diamond section.
 export const BLACK_DIAMOND = false;
+
+var initialState = {
+  counter: 0
+}
+
+function reducer(state = initialState, action) {
+  return state
+}
+
+export default reducer
---
+++ src/store.js
@@ -0,0 +1,4 @@
+import {createStore} from 'react-redux'
+import counter from './ducks/counter'
+
+export default createStore(counter)
